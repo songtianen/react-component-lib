220ca2990811935efab2d7695cc9fa06a441a22c
songtianen <songtianen@sina.com>
2020-04-24 11:23:40 +0800

    add travis

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { addDecorator, addParameters } from '@storybook/react';
+import { addDecorator, addParameters,configure } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 
 import '../src/styles/index.scss';
-
 const WrapperStyle: React.CSSProperties = {
   padding: '20px 40px',
 };
 const storyWrapperFun = (storyFn: any) => {
   return (
     <div style={WrapperStyle}>
-      <h3>组件演示</h3>
       {storyFn()}
     </div>
   );
@@ -18,3 +16,10 @@ const storyWrapperFun = (storyFn: any) => {
 addDecorator(storyWrapperFun);
 addDecorator(withInfo);
 addParameters({ info: { inline: true, header: false } });
+const loaderFn = () => {
+  const allExports=[require('../src/welcome.stories.tsx')];
+  const req = require.context('../src/components',true,/\.stories\.tsx$/);
+  req.keys().forEach(fname => allExports.push(req(fname)));
+  return allExports
+}
+configure(loaderFn,module);
---
+++ storybook-static/precache-manifest.0b66a217b4ac55f1a711adfbcd3b1a4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d9f7a3bb5a42ec48d133ab0cbbdd82",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.06064333ad39310a7e5b.bundle.js"
+  },
+  {
+    "url": "runtime~main.06064333ad39310a7e5b.bundle.js"
+  },
+  {
+    "revision": "b10b6253f2731fa12ce4",
+    "url": "static/css/main.fc7248bd.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.06064333ad39310a7e5b.bundle.js"
+  },
+  {
+    "url": "vendors~main.06064333ad39310a7e5b.bundle.js.LICENSE.txt"
+  }
+]);
