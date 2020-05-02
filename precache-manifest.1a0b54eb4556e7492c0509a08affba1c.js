8c28bf8f34b4d23be3d85c718dc091fa16c96376
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-02 21:09:35 +0000

    Deploy songtianen/react-component-lib to github.com/songtianen/react-component-lib.git:gh-pages

---
+++ precache-manifest.1a0b54eb4556e7492c0509a08affba1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a2159da010db09fe88399910a32490",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.fafdbeecb04e2a4d629d.bundle.js"
+  },
+  {
+    "url": "runtime~main.fafdbeecb04e2a4d629d.bundle.js"
+  },
+  {
+    "revision": "b31af28a32a5a75a7b4b",
+    "url": "static/css/main.fc7248bd.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.fafdbeecb04e2a4d629d.bundle.js"
+  },
+  {
+    "url": "vendors~main.fafdbeecb04e2a4d629d.bundle.js.LICENSE.txt"
+  }
+]);
